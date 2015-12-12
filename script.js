9ec73942c9f4bc5414410b38b6ece2fc224428cc
lianaschapiro <liana.schapiro@gmail.com>
2015-12-12 15:01:15 -0500

    Added pop-up lightbox functionality so pictures appear when you click on associated links

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
+	$(".lightbox-link").on("click", function(e){
+		e.preventDefault();
+		$("#text-box").text($(this).attr("alt"));
+		$("#chosen-image").attr("src", $(this).attr("src"));
+		$("#lightbox").show();
 
-
-
+			$("#lightbox").on("click", function(){
+				$("#lightbox").hide();
+			});
+	});
 });
